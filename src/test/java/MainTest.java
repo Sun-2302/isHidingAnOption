77af92ece8cd055de06542743dc75bb0659dd35f
Sun-2302 <hei.dera.932@gmail.com>
2023-08-10 15:29:36 +0300

    fix: MainTest.java

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,20 +1,20 @@
 import org.example.Main;
-import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class MainTest {
     @Test
     public void isHidingAnOptionTest(){
         //Output excepted: True
-        Assertions.assertTrue(Main.isHidingAnOption("X..C....A.."));
-        Assertions.assertTrue(Main.isHidingAnOption("XC.A"));
-        Assertions.assertTrue(Main.isHidingAnOption("X..........C...........A....C"));
+        assertTrue(Main.isHidingAnOption("X..C....A.."));
+        assertTrue(Main.isHidingAnOption("XC.A"));
+        assertTrue(Main.isHidingAnOption("X..........C...........A....C"));
 
         //Output excepted: False
-        Assertions.assertFalse(Main.isHidingAnOption("X...C..A"));
-        Assertions.assertFalse(Main.isHidingAnOption("X....A.C.....C.C"));
-        Assertions.assertFalse(Main.isHidingAnOption("X.....A"));
-        Assertions.assertFalse(Main.isHidingAnOption("X..C..A"));
+        assertFalse(Main.isHidingAnOption("X...C..A"));
+        assertFalse(Main.isHidingAnOption("X....A.C.....C.C"));
+        assertFalse(Main.isHidingAnOption("X.....A"));
+        assertFalse(Main.isHidingAnOption("X..C..A"));
     }
 
 }
